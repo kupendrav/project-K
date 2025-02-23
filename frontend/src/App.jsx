9eeb5087c50808ea63522ba1bcdbcaa7c1d0d5f6
kupendrav <kupendrav99@gmail.com>
2025-02-23 08:00:40 +0530

    add CORS support, update project title, and enhance code review feature with Markdown support

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,36 @@
-import { useState , useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import "prismjs/themes/prism-tomorrow.css"
-import Editor from 'react-simple-code-editor'
+import Editor from "react-simple-code-editor"
 import prism from "prismjs"
+import Markdown from "react-markdown"
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
 import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [code, setCode] = useState(`function sum({ return 1+1})`)
+  const [ count, setCount ] = useState(0)
+  const [ code, setCode ] = useState(` function sum() {
+  return 1 + 1
+}`)
+
+  const [ review, setReview ] = useState(``)
 
   useEffect(() => {
     prism.highlightAll()
-  },[])
+  }, [])
 
-  async function reviewCode(){
-    const reponse = await axios.post('http://localhost:5000/ai/get-review', {code})
+  async function reviewCode() {
+    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    setReview(response.data)
   }
+
   return (
     <>
       <main>
         <div className="left">
-         <div className="code">
-         <Editor
+          <div className="code">
+            <Editor
               value={code}
               onValueChange={code => setCode(code)}
               highlight={code => prism.highlight(code, prism.languages.javascript, "javascript")}
@@ -35,13 +44,18 @@ function App() {
                 width: "100%"
               }}
             />
-         </div>
-         <div 
-         onAuxClick={reviewCode}
-         
-         className="review">Review</div>
+          </div>
+          <div
+            onClick={reviewCode}
+            className="review">Review</div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <Markdown
+
+            rehypePlugins={[ rehypeHighlight ]}
+
+          >{review}</Markdown>
+        </div>
       </main>
     </>
   )
