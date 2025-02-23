5fadfa04be71e5a19d2797347d65a7f0abbc76c1
kupendrav <kupendrav99@gmail.com>
2025-02-23 07:24:38 +0530

    add axios and cors dependencies; implement code review feature with code editor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,45 @@
 import { useState , useEffect} from 'react'
 import "prismjs/themes/prism-tomorrow.css"
+import Editor from 'react-simple-code-editor'
 import prism from "prismjs"
+import axios from 'axios'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [code, setCode] = useState(`function sum({ return 1+1})`)
 
   useEffect(() => {
     prism.highlightAll()
-  })
+  },[])
+
+  async function reviewCode(){
+    const reponse = await axios.post('http://localhost:5000/ai/get-review', {code})
+  }
   return (
     <>
       <main>
         <div className="left">
          <div className="code">
-          <pre>
-            <div className="language-javascript">
-              {`function sum({
-              return 1+1})`}
-            </div>
-          </pre>
+         <Editor
+              value={code}
+              onValueChange={code => setCode(code)}
+              highlight={code => prism.highlight(code, prism.languages.javascript, "javascript")}
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                border: "1px solid #ddd",
+                borderRadius: "5px",
+                height: "100%",
+                width: "100%"
+              }}
+            />
          </div>
-         <div className="review">Review</div>
+         <div 
+         onAuxClick={reviewCode}
+         
+         className="review">Review</div>
         </div>
         <div className="right"></div>
       </main>
